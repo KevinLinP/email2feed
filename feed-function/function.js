a892864db0ecfb3e1594d3738b91853543028e31
Kevin Lin <kevin.lin.p@gmail.com>
2022-11-20 05:03:52 -0500

    generating atom feed

--- feed-function/function.js
+++ feed-function/function.js
@@ -2,6 +2,7 @@
 
 const functions = require('@google-cloud/functions-framework');
 
+const { Feed } = require('feed');
 const { initializeApp, app } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const {google} = require('googleapis');
@@ -88,6 +89,51 @@ async function fetchMessages({messageIds, auth}) {
   return messages;
 }
 
+const generateFeed = function(messages) {
+  const feed = new Feed({
+    title: "Feed Title",
+    // description: "This is my personal feed!",
+    // id: "http://example.com/",
+    // link: "http://example.com/",
+    language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+    // image: "http://example.com/image.png",
+    // favicon: "http://example.com/favicon.ico",
+    // copyright: "All rights reserved 2013, John Doe",
+    // updated: new Date(2013, 6, 14), // optional, default = today
+    // generator: "awesome", // optional, default = 'Feed for Node.js'
+    // feedLinks: {
+    //   json: "https://example.com/json",
+    //   atom: "https://example.com/atom"
+    // },
+    // author: {
+    //   name: "John Doe",
+    //   email: "johndoe@example.com",
+    //   link: "https://example.com/johndoe"
+    // }
+  });
+
+  messages.forEach((message) => {
+    const headers = {}
+
+    console.log(message.payload.headers)
+
+    message.payload.headers.forEach((header) => {
+      headers[header.name] = header.value;
+    })
+    console.log(headers);
+    console.log(message)
+
+    feed.addItem({
+      title: headers['Subject'],
+      conent: message.payload.parts[1],
+    });
+  });
+
+  return feed;
+}
+
+
+
 functions.http('function', async (req, res) => {
   const authClient = await getAuthClient();
 
@@ -97,8 +143,11 @@ functions.http('function', async (req, res) => {
   } else if (req.url.startsWith('/pragmatic-engineer')) {
     const messageIds = await listMessages(authClient)
     const messages = await fetchMessages({messageIds, auth: authClient})
-    console.log(messages)
+    const feed = generateFeed(messages);
+    response = feed.atom1();
   }
+
+  // https://www.npmjs.com/package/feed
 
   res.send(response);
 });
